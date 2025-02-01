b086d8a89a093cb618d7de39750d8c3aa26d4b86
Paras Patil <patilparas1105@gmail.com>
2025-02-01 23:36:10 +0530

    Initial commit -Ticket Booking App

---
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -0,0 +1,12 @@
+package ticket.booking.entities;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String password;
+    private String hashPassword;
+    private String userId;
+    private List<Ticket> ticketBooked;
+
+}
